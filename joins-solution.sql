19538d33f9386d3814dae2d9183b3e62c5f04e7d
Matthew Harris <matthewharrisak@gmail.com>
2020-11-03 16:41:41 -0600

    finsihed first four questions

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,20 +1,32 @@
 -- 0. Get all the users
 SELECT * FROM customers;
-```
 
---
---## Tasks
---1. Get all customers and their addresses.
+
+
+## Tasks
+--1. Get all customers and their addresses.--
+
+SELECT * FROM customers JOIN addresses 
+ON customers.id = addresses.customer_id;
 
 
 --2. Get all orders and their line items (orders, quantity and product).
-
+SELECT * from orders JOIN line_items 
+ON orders.id = line_items.order_id;
 
 --3. Which warehouses have cheetos?
-
+SELECT * FROM warehouse
+JOIN warehouse_product ON warehouse.id=warehouse_product.warehouse_id
+JOIN products ON products.id = warehouse_product.product_id
+WHERE products.description = 'cheetos';
 
 --4. Which warehouses have diet pepsi?
+SELECT * FROM warehouse
+JOIN warehouse_product ON warehouse.id=warehouse_product.warehouse_id
+JOIN products ON products.id = warehouse_product.product_id
+WHERE products.description = 'diet pepsi';
 
+Delta, Alpha, Gamma 
 
 --5. Get the number of orders for each customer. NOTE: It is OK if those without orders are not included in results.
 
